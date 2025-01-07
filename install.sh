e7b36b9e3df3a8a414da10a13da81923aa10c656
Jayabalan <jayabalanbala@gmail.com>
2025-01-06 20:26:54 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+apt-get update && apt-get upgrade -y
+apt-get install -y vim
+apt install curl apt-transport-https git wget  \
+software-properties-common lsb-release ca-certificates socat -y
+swapoff -a
+modprobe overlay
+modprobe br_netfilter
+
+cat << EOF | tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+> | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update &&  apt-get install containerd.io -y
+containerd config default | tee /etc/containerd/config.toml
+
+sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+systemctl restart containerd
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key \
+| sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" \
+| sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y kubeadm=1.30.1-1.1 kubelet=1.30.1-1.1 kubectl=1.30.1-1.1
+apt-mark hold kubelet kubeadm kubectl
+
+echo "Finding the IP address of the CP server"
+echo
+echo
+hostname -i | 
+ip addr show
+hostname -i | xargs -I {} echo "k8scp {}" >> /etc/hosts
